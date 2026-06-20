c5a28b0bfbd13a7fe81278bce3e84fbace1938b8
rama <paidakularamtej@gmail.com>
2026-06-20 17:25:50 +0530

    Started with javascript file

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 let btns = document.querySelectorAll(".innerbox");
+let body = document.querySelector("body");
 
-for (btn of btns) {
+let gamestart = false;
+//
+body.addEventListener("keypress", () => {
+  gamestart = true;
+  gamesequence = [];
+  usersequence = [];
+  generaterandomcolor();
+});
+
+function generaterandomcolor() {
+  let color = ["red", "green", "purple", "yellow"];
+  let generatedidx = Math.floor(Math.random() * color.length);
+  console.log(generatedidx);
+  gameblink(color[generatedidx]);
+}
+
+function gameblink(color) {
+  let div = document.querySelector("." + color);
+  console.dir(div);
+  div.classList.add("flash");
+  setTimeout(() => {
+    div.classList.remove("flash");
+  }, 300);
+  gamesequence.push(color);
+}
+
+for (let btn of btns) {
   btn.addEventListener("click", () => {
-    console.log(btn.classList[0]);
+    usersequence.add(btn.classList[0]);
   });
 }
+
+let gamesequence = [];
+let usersequence = [];
